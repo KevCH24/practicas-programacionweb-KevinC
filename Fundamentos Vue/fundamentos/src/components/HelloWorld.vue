e4f9483ae6ad3881b416e63b4ec277ccd13f3bad
KevCH24 <22620023@tlaxiaco.tecnm.mx>
2025-04-01 15:32:17 -0600

    Segundo commit.

---
+++ Fundamentos Vue/fundamentos/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+  <h1>Hola soy Kevin</h1>
+  <p class="parrafo">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Officia, quos suscipit! Minima dolorem voluptate qui repellendus 
+    placeat amet cumque assumenda eligendi nobis sed commodi harum 
+    modi rem, culpa molestias facilis.
+  </p>
+</template>
+
+<style scoped>
+.parrafp{
+  text-align: center;
+}
+.read-the-docs {
+  color: red;
+}
+</style>
